bb63c2df026b1c6f1aa234f0258488c84a20e903
Luis Mora <luismora371@gmail.com>
2022-04-21 20:35:32 -0500

    sample of lable statements

--- random/continue.js
+++ random/continue.js
@@ -1,10 +1,17 @@
 const names = ['luis','javi','paco','emi']
+const lasts = ['mora','test','etc']
 
-for(const name of names){
-    
+outerLoop: for(const name of names){
+    console.log(name)
     if(name === 'paco'){
         continue // skips iterations
         // break // finishes loop
     }
-    console.log(name)
+    for(const last of lasts){
+        console.log(last)
+        if (last === 'test'){
+            break outerLoop; // laber statements
+        }
+    }
+    
 }
