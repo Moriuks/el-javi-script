18cb1a521cce14883d6c02d3e05fe6db430c62ed
Luis Mora <luismora371@gmail.com>
2022-04-25 19:29:04 -0500

    functions in objects

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -1,2 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+function startGame(){
+    console.log('GameIsStarting...')
+}
+
+const person = {
+    age: 9,
+    greet: function greet (){ // function in a object is a method
+        console.log('hello there!');
+    }
+}                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                                                                                                       
+person.greet();
+console.dir(person)
+startGameBtn.addEventListener('click',startGame)
